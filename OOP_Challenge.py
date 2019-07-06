1ac315778c447ff71d3c4a8822acf4000412a5a3
Siddharth Chaudhary <siddharth.chaudhary1705@gmail.com>
2019-07-07 00:30:06 +0530

    Update OOP_Challenge.py

--- OOP_Challenge.py
+++ OOP_Challenge.py
@@ -21,7 +21,41 @@ class Account:
 
         return f"Account owner : {self.owner} \nAccount balance : ${self.balance}"
 
+    def deposit(self, deposit_amnt):
+
+        self.balance = self.balance + deposit_amnt
+        print('Deposit Accepted')
+
+    def withdraw(self, withdraw_amnt):
+
+        if self.balance < withdraw_amnt:
+            print('Sorry! Funds Unavailable')
+        else:
+            self.balance = self.balance - withdraw_amnt
+            print('Withdrawal Accepted')
+
+
 account1 = Account('Siddharth', 100)
 print(account1)
-print(account1.owner)
-print(account1.balance)
+
+while True:
+
+    ch = input('Do you wish to make a deposit? (y/n) ').lower()
+    if ch == 'n':
+        break
+    else:
+        deposit_ammount = int(input('Enter Ammount : '))
+        account1.deposit(deposit_ammount)
+
+print(account1)
+
+while True:
+
+    ch = input('Do you wish to withdraw amount? (y/n) ').lower()
+    if ch == 'n':
+        break
+    else:
+        withdrawal = int(input('Enter Amount : '))
+        account1.withdraw(withdrawal)
+
+print(account1)
